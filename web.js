661961636fd9bd7bf2f3a65d51ab1c7878888a05
Sergey Fedorov <shade30@gmail.com>
2013-08-05 15:30:15 +0000

    Added payments, image credits, email

--- web.js
+++ web.js
@@ -10,13 +10,6 @@ app.get('/', function(request, response) {
   response.send(outbuffer.toString());
 });
 
-var mailfile = "./fa87cc8b5a5b.html"
-var mailoutbuffer = new Buffer(fs.readFileSync(mailfile).toString(), "utf-8");
-
-app.get('/fa87cc8b5a5b.html', function(request, response) {
-  response.send(mailoutbuffer.toString());
-});
-
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
